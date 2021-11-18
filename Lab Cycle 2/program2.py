a2642a1f1a07fc414554bd3de7ef90ecdc971cc9
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-18 23:42:12 +0530

    Complete

---
+++ Lab Cycle 2/program2.py
@@ -0,0 +1,6 @@
+# Write a program to determine whether the character entered is a vowel or not.
+character=input("Enter a character: ")
+if character=='a' or character=='e' or character=='i' or character=='o' or character=='u' or character=='A' or character=='E' or character=='I' or character=='O' or character=='U':
+    print("The entered character is a Vowel")
+else:
+    print("The entered character is not a vowel")
