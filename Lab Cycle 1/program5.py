1a90ed943c1d28a95c12b4213c0b6263f22e9397
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-18 23:32:04 +0530

    Complete

---
+++ Lab Cycle 1/program5.py
@@ -0,0 +1,8 @@
+number1=int(input("Enter first integer number\n"))
+number2=int(input("Enter second integer number\n"))
+print("Sum of two numbers is",number1+number2)
+print("Difference of two numbers is",number1-number2)
+print("Product of two numbers is",number1*number2)
+print("Division of two numbers is",number1/number2)
+print("Modulus of two numbers is",number1%number2)
+print("Exponent of two numbers is",number1**number2)
