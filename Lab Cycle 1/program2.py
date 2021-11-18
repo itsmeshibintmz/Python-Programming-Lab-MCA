ccbf00a0fa66c2489e6e066350fd3708754428ce
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-18 23:29:21 +0530

    Complete

---
+++ Lab Cycle 1/program2.py
@@ -0,0 +1,12 @@
+integerValue=3
+print("Integer value is",integerValue)
+print("Type of integer value is",type(integerValue))
+print("ID of integer value is",id(integerValue))
+floatValue=3.14
+print("Float value is",floatValue)
+print("Type of float value is",type(floatValue))
+print("ID of float value is",id(floatValue))
+complexValue=3+4j
+print("Complex value is",complexValue)
+print("Type of complex value is",type(complexValue))
+print("ID of complex value is",id(complexValue))
