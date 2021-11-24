9f99921c1aa0d9b29395e6d62c0c8e49195d0d10
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-24 19:29:23 +0530

    Comments

--- Lab Cycle 1/program1.py
+++ Lab Cycle 1/program1.py
@@ -1,3 +1,5 @@
+# Program that prompts user to enter name display 
+# greetings message
 firstName=input("Enter your First Name\n")
 secondName=input("Enter your Second Name\n")
 print("Greetings !!!",firstName,secondName)
