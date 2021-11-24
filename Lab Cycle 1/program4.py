4fad3d6aba6919cf130bd86afeb6fb528b1cf9e6
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-24 19:35:19 +0530

    Comments

--- Lab Cycle 1/program4.py
+++ Lab Cycle 1/program4.py
@@ -1,8 +1,12 @@
+# Program to calculate salary of an employee
+
+# Employee Details
 nameOfEmployee=input("Enter name of the Employee\n")
 basicPay=float(input("Enter Basic Pay\n"))
 hra=(10/100)*basicPay
 ta=(5/100)*basicPay
 salary=basicPay+hra+ta
+# Display Employee Details with Salary
 print("Name of the Employee:",nameOfEmployee)
 print("Basic Pay:",basicPay)
 print("HRA:",hra)
