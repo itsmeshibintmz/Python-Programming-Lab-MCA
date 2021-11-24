f830134698b4b313ccb86514865e4c5da02c536f
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-24 19:40:40 +0530

    Comments

--- Lab Cycle 1/program7.py
+++ Lab Cycle 1/program7.py
@@ -1,6 +1,11 @@
+# Program to display leap years b/w two inputted years.
+
+# Prompt user for input
 startYear=int(input("Enter the start year\n"))
 endYear=int(input("Enter the end year\n"))
+#Displaying the leap years
 print("Leap years between",startYear,"and",endYear,"are:")
+# loop to check whether the year is leap year or not
 for year in range(startYear,endYear+1):
     if year%4==0 and year%100!=0 or year%400==0:
         print(year)
