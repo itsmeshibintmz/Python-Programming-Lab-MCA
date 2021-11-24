df705e1b43f3d53ded83005146105c95cefcd0c8
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-24 19:33:03 +0530

    Comments

--- Lab Cycle 1/program2.py
+++ Lab Cycle 1/program2.py
@@ -1,11 +1,17 @@
+# Program to demonstrate different number
+# data types in python
+
+# Integer value
 integerValue=3
 print("Integer value is",integerValue)
 print("Type of integer value is",type(integerValue))
 print("ID of integer value is",id(integerValue))
+# Floating point value
 floatValue=3.14
 print("Float value is",floatValue)
 print("Type of float value is",type(floatValue))
 print("ID of float value is",id(floatValue))
+# Complex value
 complexValue=3+4j
 print("Complex value is",complexValue)
 print("Type of complex value is",type(complexValue))
