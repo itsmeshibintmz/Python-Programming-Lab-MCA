08238f8cb8f90bc79fdb2f2afd9fef9554cc23bf
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-24 19:37:41 +0530

    Comments

--- Lab Cycle 1/program5.py
+++ Lab Cycle 1/program5.py
@@ -1,5 +1,9 @@
+# Program to perform Arithmetic Operations 
+
+# Prompt user for input
 number1=int(input("Enter first integer number\n"))
 number2=int(input("Enter second integer number\n"))
+# Performing operations and displaying results
 print("Sum of two numbers is",number1+number2)
 print("Difference of two numbers is",number1-number2)
 print("Product of two numbers is",number1*number2)
