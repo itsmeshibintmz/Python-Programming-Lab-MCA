d9c6318c1caf9771097e779151544f4ec4af5842
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-24 19:38:47 +0530

    Comments

--- Lab Cycle 1/program6.py
+++ Lab Cycle 1/program6.py
@@ -1,4 +1,8 @@
+# Program to check whether a given number is even or not
+
+#Prompt user for input
 numberToCheck=int(input("Enter the number to be checked\n"))
+#Check whether the number is even or not
 if numberToCheck%2==0:
     print("Number is even")
 else:
