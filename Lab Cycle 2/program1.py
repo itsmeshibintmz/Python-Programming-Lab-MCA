f4ca7bafac477edab6b444110d8559edb47a8100
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-24 19:42:10 +0530

    Comments

--- Lab Cycle 2/program1.py
+++ Lab Cycle 2/program1.py
@@ -1,7 +1,10 @@
-# Write a program to find the greatest number from three numbers.
+# Program to find the greatest number from three numbers.
+
+# Get the three numbers from the user
 number1 = int(input("Enter first number\n"))
 number2 = int(input("Enter second number\n"))
 number3 = int(input("Enter third number\n"))
+# Check which is the greatest number and display it
 if number1>number2 and number1>number3:
     print("Greatest number is",number1)
 elif number2>number1 and number2>number3:
