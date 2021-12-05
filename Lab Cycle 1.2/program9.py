5bacf5b66ff22040fab94f0196c38348f0d30d91
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-05 19:53:53 +0530

    Minor changes

--- Lab Cycle 1.2/program9.py
+++ Lab Cycle 1.2/program9.py
@@ -1,8 +1,8 @@
 num=int(input("Enter a number: "))
 temp=num
 reverse=0
-while(num>0):
-    remainder=num%10
+while(temp>0):
+    remainder=temp%10
     reverse=(reverse*10)+remainder
-    num=num//10
-print("Reverse of the number",temp,"is:",reverse)
+    temp=temp//10
+print("Reverse of the number",num,"is:",reverse)
