90113f65035aceb774d4b55d1a47a71c255d9806
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-05 19:50:28 +0530

    Using while loop

--- Lab Cycle 1.2/program9.py
+++ Lab Cycle 1.2/program9.py
@@ -1,3 +1,8 @@
 num=int(input("Enter a number: "))
-print("Reverse of the number",num,"is:",end=" ")
-print(str(num)[::-1])
+temp=num
+reverse=0
+while(num>0):
+    remainder=num%10
+    reverse=(reverse*10)+remainder
+    num=num//10
+print("Reverse of the number",temp,"is:",reverse)
