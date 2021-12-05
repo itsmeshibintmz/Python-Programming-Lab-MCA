c5bc9a45e5ba56eb20fe8f5f2af375422996d664
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-05 13:15:08 +0530

    Delete program8.py

--- Lab Cycle 2/program8.py
+++
@@ -1,8 +0,0 @@
-num1=int(input("enter first number:\n"))
-num2=int(input("Enter second number:\n"))
-while num1!=num2:
-  if num1>num2:
-    num1-=num2
-  else:
-    num2-=num1
-print("GCD is",num1)
