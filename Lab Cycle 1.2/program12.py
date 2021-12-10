bd61fc4964873d21ac336261eb59459b24bdbfe1
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:47:14 +0530

    Comments

--- Lab Cycle 1.2/program12.py
+++ Lab Cycle 1.2/program12.py
@@ -1,3 +1,11 @@
+# Program to print the multiplication table of n, 
+# where n is entered by the user.
+
+# Getting the number to perform the multiplication
 number=int(input("enter a number:\n"))
+
+# looping through the range of numbers from 1 to number
 for i in range(1,number+1):
+    # calculating the product and printing the result 
+    # in the form of multiplication table
     print(i,"x",number,"=",i*number)
