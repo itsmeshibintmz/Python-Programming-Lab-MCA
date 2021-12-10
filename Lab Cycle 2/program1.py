81397609f875b5ba95c9042b3d4be2983185b64e
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 09:54:25 +0530

    Comments added

--- Lab Cycle 2/program1.py
+++ Lab Cycle 2/program1.py
@@ -2,16 +2,23 @@
 text=input("Enter a text: ")
 print("The text is: ",text)
 print("Word count is: ")
+# Function to count the number of words in the text
 def word_count(str):
+    # Splitting the text into words
     counts = dict()
     words = str.split()
 
+    # Looping through the words
     for word in words:
+        # Checking if the word is already in the dictionary
         if word in counts:
+            # Incrementing the count of the word
             counts[word] += 1
         else:
+            # Adding the word to the dictionary
             counts[word] = 1
-
+    # Returning the dictionary
     return counts
 
+# Calling the function
 print(word_count(text))
