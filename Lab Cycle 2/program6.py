b7b62860b63e3f9548d8005258e93946bd1902a1
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 14:15:32 +0530

    Program 6

---
+++ Lab Cycle 2/program6.py
@@ -0,0 +1,7 @@
+# Program to perform List comprehensions:
+# (a) Generate positive list of numbers from a given list of integers
+# (b) Square of N numbers
+# (c) Form a list of vowels selected from a given word
+# (d) Form a list ordinal value of each element of a word (Hint: use ord() to get ordinal values)
+
+list = input("Enter a list of numbers separated by space: ").split()
