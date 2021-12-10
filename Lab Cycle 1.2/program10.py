e651d342f37fd0e4760aec66189b17581f2e4d58
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:41:42 +0530

    Comments

--- Lab Cycle 1.2/program10.py
+++ Lab Cycle 1.2/program10.py
@@ -1,11 +1,23 @@
+# Program to find whether the given number is an 
+# Amstrong number or not using while loop
+
+# Get the number from the user
 number=int(input("Enter a number: "))
+
+# Initialize sum
 sum=0
-temp=number
-while temp>0:
-    digit=temp%10
-    sum+=digit**3
-    temp//=10
-if number==sum:
+temp=number # Store the number in a temporary variable
+
+# Find the sum of the cube of each digit
+while temp>0: # Loop until the number is 0
+    digit=temp%10 # Get the last digit
+    sum+=digit**3 # Add the digit to the sum
+    temp//=10 # Remove the last digit
+
+# compare the sum with the original number
+if number==sum: 
+    # if both are equal then the number is an Amstrong number
     print(number,"is an Armstrong number")
 else:
+    # if not then the number is not an Amstrong number
     print(number,"is not an Armstrong number")
