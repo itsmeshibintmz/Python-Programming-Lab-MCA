9a36932ed41106ec7b06662f41464cd1fcaf1d02
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 14:08:07 +0530

    Algorithm

--- Lab Cycle 1/program5.py
+++ Lab Cycle 1/program5.py
@@ -12,3 +12,15 @@ print("Division of two numbers is",number1/number2)
 print("Modulus of two numbers is",number1%number2)
 print("Exponent of two numbers is",number1**number2)
 
+""""
+Algorithm
+---------
+Step 1: Get the inputs from the user
+Step 2: Find sum and display the result using sum = number1 + number2
+Step 3: Find difference and display the result using difference = number1 - number2
+Step 4: Find product and display the result using product = number1 * number2
+Step 5: Find division and display the result using division = number1 / number2
+Step 6: Find modulus and display the result using modulus = number1 % number2
+Step 7: Find exponent and display the result using exponent = number1 ** number2
+Step 8: End
+"""
