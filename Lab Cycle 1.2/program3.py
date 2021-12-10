e24e1a262e31dabc05ee8b3e44132a33d45f6fe4
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 14:55:12 +0530

    Algorithm

--- Lab Cycle 1.2/program3.py
+++ Lab Cycle 1.2/program3.py
@@ -10,3 +10,14 @@ for i in range(1,number+1):
 
 # Displaying the factorial
 print("Factorial of",number,"is",factorial)
+
+""""
+Algorithm
+---------
+Step 1: Get the number to find the factorial of
+Step 2: Initialize the factorial variable to 1
+Step 3: Loop from 1 to the number
+Step 4: Multiply the factorial variable by the current value of the loop
+Step 5: Display the factorial
+Step 6: End
+"""
