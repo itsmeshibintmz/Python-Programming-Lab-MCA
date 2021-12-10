34758795013714d746a9d6f21e3050fd1ab56705
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 17:33:20 +0530

    Algorithm

--- Lab Cycle 1.2/program12.py
+++ Lab Cycle 1.2/program12.py
@@ -9,3 +9,12 @@ for i in range(1,number+1):
     # calculating the product and printing the result 
     # in the form of multiplication table
     print(i,"x",number,"=",i*number)
+
+""""
+Algorithm
+---------
+Step 1: Get the number to perform the multiplication
+Step 2: Loop through the range of numbers from 1 to number
+Step 3: Calculate the product and print the result in the form of multiplication table (i.e. i*number=product)
+Step 4: End
+"""
