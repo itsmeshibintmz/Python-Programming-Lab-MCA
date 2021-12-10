c5da05220ca22cad6223bf845099d5e242599aa1
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 13:37:51 +0530

    Algorithm

--- Lab Cycle 1/program1.py
+++ Lab Cycle 1/program1.py
@@ -4,5 +4,14 @@
 # Prompt user to enter name
 firstName=input("Enter your First Name\n")
 lastName=input("Enter your Last Name\n")
+
 # Display greetings message
 print("Greetings !!!",firstName,lastName)
+
+""" 
+Algorithm
+----------
+Step 1: Prompt user to enter first name and last name and store it
+Step 2: Display greetings message
+Step 3: End
+"""
