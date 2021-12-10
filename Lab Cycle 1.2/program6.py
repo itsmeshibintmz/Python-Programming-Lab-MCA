040f0afd68f55dba0a8cc635949627c9739fd47f
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:28:31 +0530

    Comments

--- Lab Cycle 1.2/program6.py
+++ Lab Cycle 1.2/program6.py
@@ -1,5 +1,12 @@
+# Program to Display the given pyramid with the step number 
+# accepted from the user.
+
+# Get the number of steps from the user
 stepnum=int(input("Enter a number:\n"))
-for i in range(0,stepnum):
+
+# loop to display the pyramid
+for i in range(0,stepnum): # loop to display the pyramid
+    # loop to display the spaces
     for j in range(0,i+1):
-        print((i+1)*(j+1), end=" ")
+        print((i+1)*(j+1), end=" ") # print the number
     print('\n')
