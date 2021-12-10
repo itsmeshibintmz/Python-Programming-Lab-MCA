74874387b0685c0c1f7f33093a18f5ef94f8a162
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 17:29:29 +0530

    Algorithm

--- Lab Cycle 1.2/program10.py
+++ Lab Cycle 1.2/program10.py
@@ -21,3 +21,16 @@ if number==sum:
 else:
     # if not then the number is not an Amstrong number
     print(number,"is not an Armstrong number")
+
+""""
+Algorithm
+---------
+Step 1: Get the number from the user
+Step 2: Initialize sum as 0
+Step 3: Store the number in a temporary variable
+Step 4: Find the sum of the cube of each digit using adding the cube of each digit to the sum
+Step 5: Compare the sum with the original number
+Step 6: If both are equal then the number is an Amstrong number
+Step 7: If not then the number is not an Amstrong number
+Step 8: End
+"""
