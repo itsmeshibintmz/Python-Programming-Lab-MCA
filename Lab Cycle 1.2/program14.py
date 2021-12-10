71aa1dad5d5ff6f3131ca0f24f8e443e60f9db3e
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 11:09:29 +0530

    Comments

--- Lab Cycle 1.2/program14.py
+++ Lab Cycle 1.2/program14.py
@@ -1,11 +1,28 @@
-number=int(input("Enter a number: "))
-for i in range(number):
-    for j in range(i):
-        print ('* ', end="")
-    print('')
+# Program to Construct following pattern using nested loop
+""" 
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
 
-for i in range(number,0,-1):
-    for j in range(i):
-        print('* ', end="")
-    print('')
+# Getting the limit of the pattern
+number=int(input("Enter a number: "))
+
+# loop to print the pattern till the limit
+for i in range(number): # loop for rows 
+    for j in range(i): # loop for columns
+        print ('* ', end="") # printing the pattern with spaces
+    print('') # printing the pattern with new line
+
+# loop for printing the pattern in reverse
+for i in range(number,0,-1): # loop for rows
+    for j in range(i): # loop for columns
+        print('* ', end="") # printing the pattern with spaces
+    print('') # printing the pattern with new line
 	
