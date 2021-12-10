f5ecb8d441683b85d392b902e27f8b50b544d1d6
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:21:41 +0530

    Comments

--- Lab Cycle 1.2/program4.py
+++ Lab Cycle 1.2/program4.py
@@ -1,9 +1,18 @@
+# Getting the limit upto which fibonocci numbers 
+# need to print
 limit = int(input("Enter limit:\n"))
+
+# Initializing the first two fibonacci numbers
 first = 0
 second = 1
 third = 1
+
 print("Fibonocci Series upto",limit,"is")
+
+# Printing the first two fibonacci numbers
 print(first,end=' ')
+
+# Looping till the limit and printing the fibonacci numbers
 for i in range(0,limit):
     print(third,end=' ')
     third=first+second
