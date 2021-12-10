50585a547c12a26846fbf0fb6c2e3cbae7afaafc
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:30:55 +0530

    Comments and code update

--- Lab Cycle 1.2/program7.py
+++ Lab Cycle 1.2/program7.py
@@ -1,6 +1,14 @@
+# Program to generate all factors of a number 
+# using while loop
+
+# Get the number from the user
 number=int(input("Enter a number: "))
+
 i=1
+
+print("The factors of",number,"are:")
+# loop to find the factors
 while i<=number:
-    if number%i == 0:
-        print(i,end=" ")
-    i=i+1
+    if number%i == 0: # check if i is factor of number
+        print(i,end=" ") # if yes, print i
+    i=i+1 # increment i
