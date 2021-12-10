91c9329a0cada5deacd19684c0bf193f6c05927f
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 15:18:14 +0530

    Algorithm

--- Lab Cycle 1.2/program8.py
+++ Lab Cycle 1.2/program8.py
@@ -13,3 +13,20 @@ while num1!=num2:
     num2-=num1 # subtracting the smaller number from the larger number
 
 print("GCD is",num1) # printing the gcd
+
+""""
+Algorithm
+---------
+Step 1: Get the two numbers from the user
+Step 2: Check if the numbers are equal, if not, continue until they are equal
+Step 3: If the numbers are not equal
+          Check if the first number is greater than the second number
+            go to step 4
+            Repeat step 4 until the numbers are equal
+          Else
+            go to step 5
+            Repeat step 5 until the numbers are equal
+Step 4: Subtract the smaller number from the larger number
+Step 5: Subtract the larger number from the smaller number
+Step 6: Print the GCD result          
+"""
