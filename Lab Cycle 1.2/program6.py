260ccca323b7aba28a3a7f64b2b1154d005c88dd
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 15:05:39 +0530

    Algorithm

--- Lab Cycle 1.2/program6.py
+++ Lab Cycle 1.2/program6.py
@@ -5,8 +5,21 @@
 stepnum=int(input("Enter a number:\n"))
 
 # loop to display the pyramid
-for i in range(0,stepnum): # loop to display the pyramid
-    # loop to display the spaces
+for i in range(0,stepnum): 
+    # loop to print the value with spaces
     for j in range(0,i+1):
         print((i+1)*(j+1), end=" ") # print the number
     print('\n')
+
+""""
+Algorithm
+---------
+Step 1: Get the step number from the user
+Step 2: Loop to display the pyramid ranging from 0 to step number
+Step 3: Inner Loop to print the value with spaces
+Step 4: Print the number (i+1)*(j+1) with spaces at the end
+Step 5: End of inner loop
+Step 6: print the new line
+Step 7: End of outer loop
+Step 8: End
+"""
