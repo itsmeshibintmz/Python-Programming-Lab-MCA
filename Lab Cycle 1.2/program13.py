5c3a5e2634070eb9c3d27a5d1f4f10a8a7992387
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 17:39:37 +0530

    Algorithm

--- Lab Cycle 1.2/program13.py
+++ Lab Cycle 1.2/program13.py
@@ -11,3 +11,14 @@ for i in range (1,limit+1): # looping from 1 to length of series
 
 # Displaying the sum
 print("The sum of the series 1/2+2/3+3/4+...+n/n+1 is: ",sum)
+
+""""
+Algorithm
+---------
+Step 1: Get the length of the series
+Step 2: Initialize the sum as 0 
+Step 3: Loop from 1 to length of series
+Step 4: Add the value of i/(i+1) to sum
+Step 5: Display the sum
+Step 6: End
+"""
