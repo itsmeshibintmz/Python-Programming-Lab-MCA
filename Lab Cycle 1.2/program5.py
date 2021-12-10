9e8eaf434378f27724d25dbb97bf2db015dc1f33
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:27:00 +0530

    Comments

--- Lab Cycle 1.2/program5.py
+++ Lab Cycle 1.2/program5.py
@@ -1,5 +1,14 @@
-list = [1,2,3,4,5,6,7,8,9,10]
+# Program to find sum of all items in a list
+
+# Getting input from the user
+list_of_numbers = input("Enter a list of numbers separated by commas: ")
+
 sum=0
-for i in list:
-    sum=sum+i
-print("Sum of list items is",sum)
+
+# Converting the input string to a list
+for i in list_of_numbers.split(','):
+    # Converting the string to an integer and adding it to the sum
+    sum = sum + int(i)
+
+# Printing the sum
+print("The sum of the numbers is: ", sum)
