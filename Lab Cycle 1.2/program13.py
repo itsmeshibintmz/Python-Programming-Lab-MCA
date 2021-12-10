16b5e8ee95177d0fd5265f6b0cba5779abe4bf03
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:49:10 +0530

    Comments

--- Lab Cycle 1.2/program13.py
+++ Lab Cycle 1.2/program13.py
@@ -1,5 +1,13 @@
+# Program to sum the series - ½ + ⅔ + ……. + n/(n+1)
+
+# Getting the length of the series
 limit=int(input("Enter the limit: "))
+
 sum=0
-for i in range (1,limit+1):
-    sum+=i/(i+1)
+
+# loop to calculate the sum
+for i in range (1,limit+1): # looping from 1 to length of series
+    sum+=i/(i+1) # finding the sum
+
+# Displaying the sum
 print("The sum of the series 1/2+2/3+3/4+...+n/n+1 is: ",sum)
