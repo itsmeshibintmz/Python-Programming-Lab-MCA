1b9d334aa81bc316a16c9404785d6e4dc32defb7
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 15:08:34 +0530

    Algorithm

--- Lab Cycle 1.2/program7.py
+++ Lab Cycle 1.2/program7.py
@@ -12,3 +12,15 @@ while i<=number:
     if number%i == 0: # check if i is factor of number
         print(i,end=" ") # if yes, print i
     i=i+1 # increment i
+
+""""
+Algorithm
+---------
+Step 1: Get the number from the user
+Step 2: Initialize i to 1
+Step 3: While i is less than or equal to number
+            Check if number is divisible by i
+                If yes, print i
+            Increment i
+Step 4: End
+"""
