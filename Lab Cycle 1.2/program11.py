2a841432784a2e55800a55b13f451444da9ee597
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:44:53 +0530

    Comments

--- Lab Cycle 1.2/program11.py
+++ Lab Cycle 1.2/program11.py
@@ -1,7 +1,18 @@
+# Program to calculate the average of first n natural 
+# using for loop
+
+# Getting input from the user
 number = int(input("Enter number: "))
+
+# Initializing the sum
 sum = 0
-# loop from 1 to n
-for num in range(1, number + 1, 1):
-    sum = sum + num
+
+# loop to calculate the sum
+for num in range(1, number + 1, 1): # looping from 1 to number
+    sum = sum + num # adding the number to the sum
+
+# calculating the average
 average = sum / number
+
+# printing the result
 print("Average of ", number, "numbers is: ", average)
