fa28c7fcfe0bbce4a68ab3321b347ddb466a0836
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 15:00:57 +0530

    Algorithm

--- Lab Cycle 1.2/program5.py
+++ Lab Cycle 1.2/program5.py
@@ -12,3 +12,14 @@ for i in list_of_numbers.split(','):
 
 # Printing the sum
 print("The sum of the numbers is: ", sum)
+
+""""
+Algorithm
+---------
+Step 1: Get the list input from the user
+Step 2: Convert the input string to a list
+Step 3: Initialize the sum variable to 0
+Step 4: Loop through the list and add each item to the sum
+Step 5: Print the sum
+Step 6: End
+"""
