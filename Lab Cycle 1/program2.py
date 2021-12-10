73e36492eac5ce564dcb3c0021fe32e6ba41c8a8
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:03:33 +0530

    Comments

--- Lab Cycle 1/program2.py
+++ Lab Cycle 1/program2.py
@@ -6,15 +6,19 @@ integerValue=3
 print("Integer value is",integerValue)
 # Printing the type of integer value
 print("Type of integer value is",type(integerValue))
-# Printing 
+# Printing the id of integer value
 print("ID of integer value is",id(integerValue))
 # Floating point value
 floatValue=3.14
 print("Float value is",floatValue)
+# Printing the type of float value
 print("Type of float value is",type(floatValue))
+# Printing the id of float value
 print("ID of float value is",id(floatValue))
 # Complex value
 complexValue=3+4j
 print("Complex value is",complexValue)
+# Printing the type of complex value
 print("Type of complex value is",type(complexValue))
+# Printing the id of complex value
 print("ID of complex value is",id(complexValue))
