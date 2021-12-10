b83f7999bf031324109bc3b77a6f1a51703f0092
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 13:38:33 +0530

    Algorithm

--- Lab Cycle 1/program3.py
+++ Lab Cycle 1/program3.py
@@ -9,3 +9,14 @@ radius=int(input("Enter radis of circle\n"))
 # Calculate the area of the circle
 area=PI*radius*radius
 print("Area of circle is",area)
+
+""""
+Algorithm
+---------
+Step 1: Define the pi constant
+Step 2: Get the radius from the user
+Step 3: Calculate the area of the circle using 
+        formula area=pi*radius*radius
+Step 4: Print the area of the circle
+Step 5: End
+"""
