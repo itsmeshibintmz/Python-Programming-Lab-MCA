c90c73e818a94901197b49395ee13be049d1659a
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:18:38 +0530

    Comments

--- Lab Cycle 1.2/program3.py
+++ Lab Cycle 1.2/program3.py
@@ -1,10 +1,12 @@
 # Program to find the factorial of a number.
 
-# Prompt user for input
+# Getting input from the user
 number= int(input("Enter a number\n"))
 factorial=1
-# Calculate the factorial
+
+# Calculating the factorial
 for i in range(1,number+1):
     factorial=factorial*i
-# Display the factorial
+
+# Displaying the factorial
 print("Factorial of",number,"is",factorial)
