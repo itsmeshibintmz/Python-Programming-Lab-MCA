1ff00b2c7d29c1ad03074db5f36ab439a91aaf13
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-10 10:33:11 +0530

    Comments

--- Lab Cycle 1.2/program8.py
+++ Lab Cycle 1.2/program8.py
@@ -1,8 +1,15 @@
+# Program to find gcd of 2 numbers using while loop
+
+# Getting inputs from the user
 num1=int(input("enter first number:\n"))
 num2=int(input("Enter second number:\n"))
+
+# checking if the numbers are equal
 while num1!=num2:
-  if num1>num2:
-    num1-=num2
+  # finding the smaller number
+  if num1>num2: 
+    num1-=num2 # subtracting the smaller number from the larger number
   else:
-    num2-=num1
-print("GCD is",num1)
+    num2-=num1 # subtracting the smaller number from the larger number
+
+print("GCD is",num1) # printing the gcd
