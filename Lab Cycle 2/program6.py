5434463acee0e9ed81f17dd4945abfe16fb7b2e3
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-11 07:10:38 +0530

    Positive list

--- Lab Cycle 2/program6.py
+++ Lab Cycle 2/program6.py
@@ -4,4 +4,18 @@
 # (c) Form a list of vowels selected from a given word
 # (d) Form a list ordinal value of each element of a word (Hint: use ord() to get ordinal values)
 
-list = input("Enter a list of numbers separated by space: ").split()
+def positive_list(list):
+    return [i for i in list if i>0]
+
+def square_list(list):
+    return [i**2 for i in list]
+
+def vowel_list(word):
+    return [i for i in word if i in "aeiouAEIOU"]
+
+def ordinal_list(word):
+    return [ord(i) for i in word]
+
+print("Enter a list of numbers to generate positive list: ")
+list = [int(i) for i in input().split()]
+print("Positive list: ", positive_list(list))
