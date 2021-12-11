6c5c7e7a84f60b068deab239159622de3827be69
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-11 22:54:59 +0530

    Program 17

--- Lab Cycle 2/program17.py
+++ Lab Cycle 2/program17.py
@@ -1,14 +1,15 @@
 # Program to accept a list of words and return the length of the longest word.
 
+# Function to find the ength of the longest word 
 def longest_word(words):
-    longest = 0
-    for word in words:
-        if len(word) > longest:
-            longest = len(word)
-            longest_word = word
-    print("Longest word is",longest_word)
-    print("Length of longest word is",longest)
+    longest = 0 # Initialize the longest word length to 0
+    for word in words: 
+        if len(word) > longest: # If the length of the current word is greater than the longest word length
+            longest = len(word) # Set the longest word length to the length of the current word
+            longest_word = word # Set the longest word to the current word
+    print("Longest word is",longest_word) # Print the longest word
+    print("Length of longest word is",longest) # Print the length of the longest word
 
-print("Enter a list of words separated by spaces: ")
-words = input().split()
-longest_word(words)
+print("Enter a list of words separated by spaces: ") 
+words = input().split() # Get the list of words from the user
+longest_word(words) # Function call
